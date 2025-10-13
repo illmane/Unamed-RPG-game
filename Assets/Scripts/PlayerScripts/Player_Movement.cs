f146419d8df066e2f579a05ae8c8ff35921e0a72
illmane <illmane974@gmail.com>
2025-10-13 20:59:52 +0100

    IdleAnimations-completed
    Completed all idle animations in all directions

--- Assets/Scripts/PlayerScripts/Player_Movement.cs
+++ Assets/Scripts/PlayerScripts/Player_Movement.cs
@@ -3,10 +3,15 @@ using UnityEngine;
 public class Player_Movement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private float moveXValue;
+    private float moveYValue;
+    private Animator Anim;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+        Anim = gameObject.GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
@@ -19,6 +24,32 @@ public class Player_Movement : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+
+        if (horizontal < -0.05f)
+        {
+            moveXValue = -1;
+            moveYValue = 0f;
+        }
+        else if (horizontal > 0.05f)
+        {
+            moveXValue = 1;
+            moveYValue = 0f;
+        }
+
+        if (vertical < -0.05f)
+        {
+            moveXValue = 0;
+            moveYValue = -1f;
+        }
+        else if (vertical > 0.05f)
+        {
+            moveXValue = 0;
+            moveYValue = 1f;
+        }
+
+        Anim.SetFloat("MoveX", moveXValue);
+        Anim.SetFloat("MoveY", moveYValue);
+
 
         rb.linearVelocity = new Vector2(horizontal * StatsManager.Instance.MovementSpeed, vertical * StatsManager.Instance.MovementSpeed);
 
