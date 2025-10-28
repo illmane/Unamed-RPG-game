723adb558ec4ff91576ddd22e5b05df3b914be6c
illmane <illmane974@gmail.com>
2025-10-28 20:49:26 +0000

    added health bar ui slider

--- Assets/Scripts/PlayerScripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerScripts/PlayerHealth.cs
@@ -1,15 +1,22 @@
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class PlayerHealth : MonoBehaviour
 {
+    public Slider HealthSlider;
     void Start()
     {
         StatsManager.Instance.currentHealh = StatsManager.Instance.maxHealth;
+        HealthSlider.maxValue = StatsManager.Instance.maxHealth;
+        HealthSlider.value = StatsManager.Instance.currentHealh;
     }
 
     public void takeDamage(float damageAmount)
     {
         StatsManager.Instance.currentHealh -= damageAmount;
+        HealthSlider.value = StatsManager.Instance.currentHealh;
+
 
         if (StatsManager.Instance.currentHealh <= 0f)
         {
