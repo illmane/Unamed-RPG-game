78974b411b2e53eaac8fb5b6ba37e5d84efd6773
illmane <illmane974@gmail.com>
2025-11-06 21:15:30 +0000

    made enemy AI smarter
    added ability to flip, chasing animation and created a state machine

--- Assets/Scripts/Enemy_AI.cs
+++ Assets/Scripts/Enemy_AI.cs
@@ -1,23 +1,37 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
+
+public enum EnemyState
+{
+    Idle,
+    Chasing
+}
 public class Enemy_AI : MonoBehaviour
 {
     public float damageAmount = 2f;
     public float enemySpeed;
-
+    public EnemyState state;
 
     private Rigidbody2D rb;
+    private Animator anim;
     private Transform _Player;
-    private bool isChasing;
+    private float facingDirection = 2.2f;
 
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
+        // ChangeState(EnemyState.Idle);
+        state = EnemyState.Idle;
     }
 
     void FixedUpdate()
     {
-        enemyChasing();
+        if (state == EnemyState.Chasing)
+        {
+            enemyChasing();
+        }
     }
 
     // damage player if touched (active hitbox)
@@ -28,17 +42,20 @@ public class Enemy_AI : MonoBehaviour
             collision.gameObject.GetComponent<PlayerHealth>().takeDamage(damageAmount);
         }
     }
-
     private void enemyChasing()
     {
-        if (isChasing == true)
+        if (_Player.position.x > transform.position.x && facingDirection == -2.2f || _Player.position.x < transform.position.x && facingDirection == 2.2f)
         {
-            Vector2 movementDirection = (_Player.position - transform.position).normalized;
-            rb.linearVelocity = movementDirection * enemySpeed;
-            
+            EnemyFlip();
         }
+        Vector2 movementDirection = (_Player.position - transform.position).normalized;
+        rb.linearVelocity = movementDirection * enemySpeed;
     }
-
+    private void EnemyFlip()
+    {
+        facingDirection *= -1;
+        transform.localScale = new Vector3(facingDirection, transform.localScale.y, transform.localScale.z);
+    }
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
@@ -47,16 +64,33 @@ public class Enemy_AI : MonoBehaviour
             {
                 _Player = collision.transform;
             }
-            isChasing = true;
+            ChangeState(EnemyState.Chasing);
         }
     }
-
     void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             rb.linearVelocity = Vector2.zero;
-            isChasing = false;
+            ChangeState(EnemyState.Idle);
         }
+    }
+
+    private void ChangeState(EnemyState newState)
+    {
+        //exit the current state
+        if (state == EnemyState.Chasing)
+        {
+            anim.SetBool("isChasing", false);
+        }
+        // updates the state
+        state = newState;
+
+        // enter the new state animation
+        if (state == EnemyState.Chasing)
+        {
+            anim.SetBool("isChasing", true);
+        }
+
     }
 }
