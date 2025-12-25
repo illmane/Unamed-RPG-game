49247aaa0438e811b4f4d4525670a9224df8e597
illmane <illmane974@gmail.com>
2025-12-25 10:20:38 +0000

    fixed the attack point positioning

--- Assets/Scripts/PlayerScripts/Player_Combat.cs
+++ Assets/Scripts/PlayerScripts/Player_Combat.cs
@@ -4,6 +4,7 @@ public class Player_Combat : MonoBehaviour
 {
     private Animator anim;
     private float timer;
+    private float ATTACKPOINT_POSITION = 0.7f; 
 
     public Transform attackPoint;
     public LayerMask enemyLayer;
@@ -20,7 +21,6 @@ public class Player_Combat : MonoBehaviour
             timer -= Time.deltaTime;
         }
     }
-
     public void player_attack(float moveXValue)
     {
         if (timer <= 0f)
@@ -28,19 +28,19 @@ public class Player_Combat : MonoBehaviour
             if (moveXValue == 1 || moveXValue == 0)
             {
                 anim.SetFloat("AttackX", 1);
+                attackPoint.localPosition = new Vector3(ATTACKPOINT_POSITION, attackPoint.localPosition.y, attackPoint.localPosition.z);
             }
             else if (moveXValue == -1)
             {
                 anim.SetFloat("AttackX", 0);
+                attackPoint.localPosition = new Vector3(-ATTACKPOINT_POSITION, attackPoint.localPosition.y, attackPoint.localPosition.z);
             }
-
             anim.SetBool("isAttacking", true);
             Collider2D[] enemies = Physics2D.OverlapCircleAll(attackPoint.position, StatsManager.Instance.attackRange, enemyLayer);
             foreach(Collider2D enemy in enemies)
             {
                 if(enemy.isTrigger) continue;
             }
-            
             if (enemies.Length > 0)
             {
                 enemies[0].GetComponent<Enemy_Health>().takeDamage();
