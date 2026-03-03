2607f0c1401ad82818762f893f68ec0cf979b13a
illmane <illmane974@gmail.com>
2026-03-03 10:32:52 +0000

    added rock enemy with some basic mechanics
    need to make a dedicated attack animation. Need to join the roll-up and rolling actions into one action.

--- Assets/Scripts/EnemyScripts/Enemy_Combat.cs
+++ Assets/Scripts/EnemyScripts/Enemy_Combat.cs
@@ -12,14 +12,6 @@ public class Enemy_Combat : MonoBehaviour
         attackPoint = gameObject.GetComponentInChildren<Transform>();
         
     }
-
-    // void OnCollisionEnter2D(Collision2D collision)
-    // {
-    //     if (collision.gameObject.tag == "Player")
-    //     {
-    //         collision.gameObject.GetComponent<PlayerHealth>().takeDamage(-damageAmount);
-    //     }
-    // }
     
     public void Attack()
     {
--- Assets/Scripts/EnemyScripts/RockEnemy_AI.cs
+++ Assets/Scripts/EnemyScripts/RockEnemy_AI.cs
@@ -1,19 +1,18 @@
-using Unity.VisualScripting;
+using System;
 using UnityEngine;
 
-
-public enum EnemyState
+public enum RockEnemyState
 {
     Idle,
     Chasing,
     Attacking
 }
-public class Enemy_AI : MonoBehaviour
+public class RockEnemy_AI : MonoBehaviour
 {
     public float enemySpeed;
-    public EnemyState state;
+    public RockEnemyState state;
     public float attackDistance;
-    public float playerDetectionRange = 2.3f;
+    public float playerDetectionRange;
     public Transform detectionPoint;
     public LayerMask PlayerLayer;
     public float attackCooldown;
@@ -21,31 +20,33 @@ public class Enemy_AI : MonoBehaviour
     private Rigidbody2D rb;
     private Animator anim;
     private Transform _Player;
-    private float facingDirection = 2.2f;
+    private float facingDirection = 1.33f;
     private float attackCooldownTimer;
-
+    
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
         anim = gameObject.GetComponent<Animator>();
-        state = EnemyState.Idle;
+        state = RockEnemyState.Idle;
+
 
     }
 
+    // Update is called once per frame
     void Update()
     {
         if (attackCooldownTimer > 0)
         {
             attackCooldownTimer -= Time.deltaTime;
         }
-        // constantly check if player is within range
+
         CheckForPlayer();
 
-        if (state == EnemyState.Chasing)
+        if (state == RockEnemyState.Chasing)
         {
             enemyChasing();
         }
-        else if (state == EnemyState.Attacking)
+        else if (state == RockEnemyState.Attacking)
         {
             rb.linearVelocity = Vector2.zero;
         }
@@ -53,7 +54,7 @@ public class Enemy_AI : MonoBehaviour
 
     private void enemyChasing()
     {
-        if (_Player.position.x > transform.position.x && facingDirection == 2.2f || _Player.position.x < transform.position.x && facingDirection == -2.2f)
+        if (_Player.position.x > transform.position.x && facingDirection == 1.33f || _Player.position.x < transform.position.x && facingDirection == -1.33f)
         {
             EnemyFlip();
         }
@@ -61,17 +62,17 @@ public class Enemy_AI : MonoBehaviour
         Vector2 movementDirection = (_Player.position - transform.position).normalized;
         rb.linearVelocity = movementDirection * enemySpeed;
     }
+
     private void EnemyFlip()
     {
         facingDirection *= -1;
         transform.localScale = new Vector3(facingDirection, transform.localScale.y, transform.localScale.z);
     }
-
     private void CheckForPlayer()
     {
         // If player is within detection range
         Collider2D[] hits = Physics2D.OverlapCircleAll(detectionPoint.position, playerDetectionRange, PlayerLayer);
-
+        
         if (hits.Length > 0)
         {
             _Player = hits[0].transform;
@@ -79,33 +80,52 @@ public class Enemy_AI : MonoBehaviour
             // If player is within detection AND attack range
             if (Vector2.Distance(transform.position, _Player.position) <= attackDistance && attackCooldownTimer <= 0){
 
+                if (_Player.position.x > transform.position.x)
+                {
+                    print("player is RIGHT of me");
+                }
+                else if (_Player.position.x < transform.position.x)
+                {
+                    print("player is LEFT of me");
+                }
+
+                if (_Player.position.y < transform.position.y)
+                {
+                    print("player is BELOW me");
+                }
+                else if (_Player.position.y > transform.position.y)
+                {
+                    print("player is ABOVE me");
+                    
+                }
                 attackCooldownTimer = attackCooldown;
-                ChangeState(EnemyState.Attacking);
+
+                ChangeState(RockEnemyState.Attacking);
             }
-            else if (Vector2.Distance(transform.position, _Player.position) > attackDistance && state != EnemyState.Attacking)
+            else if (Vector2.Distance(transform.position, _Player.position) > attackDistance && state != RockEnemyState.Attacking)
             {
-                ChangeState(EnemyState.Chasing);
+                ChangeState(RockEnemyState.Chasing);
             }
         }
         else
         {
             rb.linearVelocity = Vector2.zero;
-            ChangeState(EnemyState.Idle);
+            ChangeState(RockEnemyState.Idle);
         }
     }
 
-    private void ChangeState(EnemyState newState)
+    private void ChangeState(RockEnemyState newState)
     {
         //exit the current state
-        if (state == EnemyState.Chasing)
+        if (state == RockEnemyState.Chasing)
         {
             anim.SetBool("isChasing", false);
         }
-        else if (state == EnemyState.Attacking)
+        else if (state == RockEnemyState.Attacking)
         {
             anim.SetBool("isAttacking", false);
         }
-        else if (state == EnemyState.Idle)
+        else if (state == RockEnemyState.Idle)
         {
             anim.SetBool("isIdle", false);
         }
@@ -113,15 +133,15 @@ public class Enemy_AI : MonoBehaviour
         state = newState;
 
         // enter the new state animation
-        if (state == EnemyState.Chasing)
+        if (state == RockEnemyState.Chasing)
         {
             anim.SetBool("isChasing", true);
         }
-        else if (state == EnemyState.Attacking)
+        else if (state == RockEnemyState.Attacking)
         {
             anim.SetBool("isAttacking", true);
         }
-        else if (state == EnemyState.Idle)
+        else if (state == RockEnemyState.Idle)
         {
             anim.SetBool("isIdle", true);
         }
