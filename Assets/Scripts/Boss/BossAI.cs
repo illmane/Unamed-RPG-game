bc3fa9ce59f1d017d71ef5acf37bbc186bfc587d
illmane <illmane974@gmail.com>
2026-06-03 13:30:18 +0100

    boss AOE somewhat working

--- Assets/Scripts/Boss/BossAI.cs
+++ Assets/Scripts/Boss/BossAI.cs
@@ -132,5 +132,9 @@ public class BossAI : MonoBehaviour
         {
             attackCooldownTimer = AOE_cooldown;
         }
+        else if (rand == 0)
+        {
+            return;
+        }
     }
 }
--- Assets/Scripts/Boss/InstantiateFireball.cs
+++ Assets/Scripts/Boss/InstantiateFireball.cs
@@ -15,7 +15,7 @@ public class InstantiateFireball : MonoBehaviour
 
     private void CreateFireballs()
     {
-        randAmount = Random.Range(0, 6);
+        randAmount = Random.Range(5, 13);
         for (int i = 0; i < randAmount; i++){
         Instantiate(FireballPrefab, new Vector3(Random.Range(-9.72f, 10.14f), Random.Range(-5.73f, 5.48f), 0), Quaternion.identity);
         StartCoroutine(DelayFireballSpawn());
