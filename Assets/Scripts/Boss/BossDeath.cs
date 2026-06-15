19ff70df1a063df544f54e51e639ab97b8407873
illmane <illmane974@gmail.com>
2026-06-15 21:27:32 +0100

    added some mechanic when boss dies

--- Assets/Scripts/Boss/BossDeath.cs
+++ Assets/Scripts/Boss/BossDeath.cs
@@ -2,14 +2,25 @@ using UnityEngine;
 
 public class BossDeath : MonoBehaviour
 {
-
+    private GameObject ExitTiles;
     void Awake()
     {
         Enemy_Health.OnKillingBoss += BossKilled;
     }
 
+    void Start()
+    {
+        ExitTiles = GameObject.FindGameObjectWithTag("ExitTile");
+    }
+
     private void BossKilled()
     {
         print("Boss has been killed");
+        ExitTiles.SetActive(false);
+    }
+
+    void OnDestroy()
+    {
+        Enemy_Health.OnKillingBoss -= BossKilled;
     }
 }
